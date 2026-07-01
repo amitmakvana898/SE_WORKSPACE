5c9c795db1c65d73bb12bd014504ac8c9d56e698
amitmakvana898 <amitmakvana898@gmail.com>
2026-07-01 11:32:07 +0530

    Add files via upload

---
+++ Module 2/program2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a=10;
+    int b=20;
+    int add=a+b;
+    int sub=a-b;
+    int mul=a*b;
+    int division=a/b;
+    printf("\n addtion of two number %d",add);
+    printf("\n sub of two number %d",sub);
+    printf("\n mul of two number %d",mul);
+    printf("\n division of two number %d",division);
+    getch();
+}
