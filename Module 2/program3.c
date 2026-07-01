f9ac742980853827739344b01acd0967011baa10
amitmakvana898 <amitmakvana898@gmail.com>
2026-07-01 11:32:29 +0530

    Add files via upload

---
+++ Module 2/program3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a=10;
+    int square=a*a;
+    int cube=a*a*a;
+    printf("\n square is %d",square);
+    printf("\n cube is %d",cube);
+    getch();
+}
