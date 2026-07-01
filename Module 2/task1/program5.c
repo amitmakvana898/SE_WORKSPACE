c928d7832c9c5f70057c81bbe1fb701abf14d340
amitmakvana898 <amitmakvana898@gmail.com>
2026-07-01 11:49:49 +0530

    module2

---
+++ Module 2/task1/program5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    int base = 10;
+    int height = 21;
+    int area = (base * height) / 2;
+
+    printf("Area of triangle = %d", area);
+
+    getch();
+}
---
+++ Module 2/task1/program6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    int p=10;
+    int r=20;
+    int i=2;
+    int si=p*i*r/100;
+    printf("si valus is %d",si);
+    getch();
+    
+}
