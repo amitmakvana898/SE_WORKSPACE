fc5f28498914b9e0b0f8024519acb8fe3e76ad13
amitmakvana898 <amitmakvana898@gmail.com>
2026-07-01 11:45:33 +0530

    Add files via upload

---
+++ Module 2/program7.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    int maths=50;
+    int scince=70;
+    int gujrati=80;
+    int english=76;
+    int hindi=90;
+    int sub=maths+scince+gujrati+english+hindi;
+    printf("\n total marks%d",sub);
+    float per=sub/5;
+    printf("\npercetage is %f",per);
+   
+    getch();
+    
+}
